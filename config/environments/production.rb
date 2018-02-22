c92d1fd1d073c3928bc146cbe6d8d058b2817c44
Matsor Browncoat <prunkstation@gmail.com>
2018-02-22 19:10:00 +0100

    🔨 Testing nginx configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://kendraw.nerdship.net/ws'
   config.action_cable.allowed_request_origins = [ 'https://rubybrowncoat.github.io', /https:\/\/keydraw.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
